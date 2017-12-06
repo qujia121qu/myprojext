e4fcb7deccfd3bcfd36f359f3905c725effd655d
qujia <545531210@qq.com>
2017-12-06 15:49:59 +0800

    Asdasd

---
+++ MyProject/src/com/test/pojo/User.java
@@ -0,0 +1,26 @@
+package com.test.pojo;
+
+public class User {
+	int id;
+	String name;
+	String sex;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
